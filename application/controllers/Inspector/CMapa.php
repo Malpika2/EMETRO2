82c2730cbc9aa8fc5808978b456453c464a35391
Karlos666 <34383458+Karlos666@users.noreply.github.com>
2017-12-18 13:10:23 -0600

    Mapa
    soy el mapa

--- application/controllers/Inspector/CMapa.php
+++ application/controllers/Inspector/CMapa.php
@@ -2,11 +2,39 @@
 /**
 * 
 */
-class cMapa extends AnotherClass
+class cMapa extends CI_Controller
 {
 	
-	function __construct(argument)
+	function __construct()
 	{
-		# code...
+		parent::__construct();
+		$this->load->model('Inspector/mMapa');
+	}
+	
+	public function index()
+	{
+		$this->load->view('Inspector/vHeader');
+		$this->load->view('Inspector/vMenu');
+		$this->load->view('Inspector/Inspeccion/vMapa');
+		$this->load->view('Inspector/vFooter');
+	}
+	public function grabar_punto()
+	{
+		$param['titulo'] = $this->input->post('titulo');
+		$param['descripcion'] = $this->input->post('descripcion');
+		$param['cx'] = $this->input->post('cx');
+		$param['cy'] = $this->input->post('cy');
+		
+		$this->mMapa->grabar_punto($param);
+	}
+
+	public function ver_punto()
+	{
+		echo json_encode($this->mMapa->ver_punto());
+	}
+
+	public function ver_marcador()
+	{
+		echo json_encode($this->mMapa->ver_marcador());
 	}
 }
